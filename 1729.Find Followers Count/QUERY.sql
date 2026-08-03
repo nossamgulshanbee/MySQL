8c56cd954e5787b6ce7ae3f07a222967bcc866cb
Nossam Gulshanbee <168060832+nossamgulshanbee@users.noreply.github.com>
2026-08-03 22:37:35 +0530

    Update QUERY.sql

--- 1729.Find Followers Count/QUERY.sql
+++ 1729.Find Followers Count/QUERY.sql
@@ -1,1 +1,1 @@
-SELECT user_id,COUNT(follower_id) as followers_count FROM Followers GROUP BY user_id ;
+SELECT user_id,COUNT(follower_id) as followers_count FROM Followers GROUP BY user_id  order BY user_id;

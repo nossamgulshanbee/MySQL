ccf12e21807723b7793b26deebe5d385efde32f8
Nossam Gulshanbee <168060832+nossamgulshanbee@users.noreply.github.com>
2026-08-06 21:31:39 +0530

    Create QUERY.sql

---
+++ 1084.Sales Analysis lll/QUERY.sql
@@ -0,0 +1,5 @@
+SELECT s.product_id,p.product_name 
+FROM Product p JOIN Sales s 
+ON p.product_id=s.product_id 
+GROUP BY p.product_id 
+HAVING MIN(sale_date)>='2019-01-01' and MAX(sale_date)< '2019-03-31';
